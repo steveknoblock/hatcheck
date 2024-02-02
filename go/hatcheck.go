c755276cf6f38beacc720139ca6162768698e67a
Steve Knoblock <steve.knoblock@gmail.com>
2024-02-01 23:29:07 -0500

    wtf

--- go/hatcheck.go
+++ go/hatcheck.go
@@ -1,40 +1,32 @@
 package main
 
 import (
-  "crypto/md5"
-  "fmt"
-  "bufio"
-  "io/ioutil"
-  "os"
- )
- 
+	"fmt"
+	"os"
+)
+
 func check(e error) {
-  if e != nil {
-      panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-   //data = read file here
-   //hash = md5.sum(data)
+	//data = read file here
+	//hash = md5.sum(data)
 
-  data = "Hello World"
+	data := "Hello World"
 
-  hash = md5.sum(data)
+	hash := md5.sum(data)
 
-  f, e := os.Create(hash)
-  check(e)
+	f, e := os.Create(hash)
+	check(e)
 
-  l, e := f.WriteString(data)
-    if e != nil {
-      fmt.Println(e)
-      f.Close()
-      return
-  }
+	l, e := f.WriteString(data)
+	if e != nil {
+		fmt.Println(e)
+		f.Close()
+		return
+	}
 
-
-
-
- }
- 
- 
+}

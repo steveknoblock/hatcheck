6394ff28f2a2543fef6f6bc093a300b7d08abd88
Steve Knoblock <steve.knoblock@gmail.com>
2026-03-08 15:53:00 -0400

    Missed main.go

--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	//"io"
 	"os"
-	//"log"
 	"crypto/md5"
 	"fmt"
-	//"path/filepath"
 	"cas/cas"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 data := "Hello World 255" // Text data string
 
